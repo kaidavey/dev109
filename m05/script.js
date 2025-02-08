34de4014846adefd8487b60a4e121b5f5d04c7d8
Kai <k.davey4@icloud.com>
2025-02-07 23:30:21 -0800

    prompted user + calculated message

---
+++ m05/script.js
@@ -0,0 +1,34 @@
+// Prompt the user to enter a number
+let num = prompt("Please enter a number (0-10)", "3");
+
+// Convert the string input to a number
+num = Number(num);
+
+// Pass the number through checks to make sure it is valid
+if (num == null || isNaN(num) || num < 0 || num > 10) {
+  console.log("You entered a value that does not fit the parameters.");
+  num = 0;
+}
+
+/*
+if (operator === 'addition') {
+  // Do addition
+  while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+    i++;
+  }
+} else {
+*/
+
+// Declare the message
+var msg = '<h2>Multiplication Table</h2>';
+
+// Calculate the Multiplication Table
+for (var i = 1; i <= 10; i++) {
+  msg += i + ' x ' + num + ' = ' + (i * num) + '<br>';
+  i++;
+}
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
