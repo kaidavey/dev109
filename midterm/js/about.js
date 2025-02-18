fb1a94ca021ceffb40fa391e7bc21b516a74eaa1
Kai <k.davey4@icloud.com>
2025-02-17 21:33:34 -0500

    added js function to about page

--- midterm/js/about.js
+++ midterm/js/about.js
@@ -1,11 +1,5 @@
 // Create var for message
-let msg = document.getElementById('message');
-
-// Create Date Object for today and set min, sec, ms to 0
-let today = new Date();
-today.setMinutes(0);
-today.setSeconds(0);
-today.setMilliseconds(0);
+let msg = document.getElementById('result');
 
 // Function Called by HTML file
 function calculateDifference(inputDate) {
@@ -17,37 +11,16 @@ function calculateDifference(inputDate) {
     // Create Date Object for user input
     let userDate = new Date(inputDate);
 
-    if (validateDate(userDate)) {
-        let userYear = userDate.getFullYear();
-        let diffYear = userYear - 2007;
+    let userYear = userDate.getFullYear();
+    let diffYear = userYear - 2029;
 
-        let diffMillisecond = userDate - new Date(2007, 6, 22);
-        let diffDay = Math.round(diffMillisecond / (1000 * 60 * 60 * 24));
+    msg.innerHTML = 'An error occurred.'
 
-        let comparison = document.createElement('p');
-        comparison.innerHTML = 'An error occurred.'
-
-        if (diffDay > 0) {
-            comparison.innerHTML = "You are " + diffYear + " years or " + diffDay + " days younger than Kai!";
-        } else if (diffDay < 0) {
-            comparison.innerHTML = "You are " + Math.abs(diffYear) + " years or " + Math.abs(diffDay) + " days older than Kai!";
-        } else {
-            comparison.innerHTML = "You were born on the same day as Kai!";
-        }
-
-        msg.appendChild(comparison);
+    if (diffYear > 0) {
+        msg.innerHTML = "You will graduate " + diffYear + " years after me.";
+    } else if (diffYear < 0) {
+        msg.innerHTML = "I will be graduating " + Math.abs(diffYear) + " years after you.";
+    } else {
+        msg.innerHTML = "We are in the same graduating class! Let's get to know each other.";
     }
-}
-
-// Helper function for validating date
-function validateDate(userDate) {
-    if (userDate.getTime() > today.getTime()) {
-        let error = document.createElement('p');
-        error.innerHTML = "You can't be born in the future! Try again.";
-
-        // Add error message to message div
-        msg.appendChild(error);
-        return false;
-    }
-    return true;
 }
