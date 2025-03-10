6b286fa6996e9b7da85632df99d94afc8ecc5668
Kai <k.davey4@icloud.com>
2025-03-09 23:44:05 -0700

    initial commit

---
+++ a5/js/script.js
@@ -0,0 +1,59 @@
+var imgs = document.getElementsByTagName("img");
+var timer = document.getElementById("timer");
+var currImg = 0;
+var currTime = 4;
+var interval = setInterval(nextImage, 4000);
+var countdown = setInterval(updateTimer, 1000);
+changeImg(currImg);
+
+function changeImg(index) {
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].style.display = "none";
+    }
+    imgs[index].style.display = "block";
+    currImg = index;
+    resetTimer();
+}
+
+function updateTimer() {
+    if (currTime > 0) {
+        currTime--;
+    }
+    timer.textContent = currTime;
+}
+
+function resetTimer() {
+    currTime = 4;
+    timer.textContent = currTime;
+}
+
+function resetInterval() {
+    clearInterval(interval);
+    interval = setInterval(nextImage, 4000);
+}
+
+function previousImage() {
+    if (currImg === 0) {
+        changeImg(imgs.length - 1);
+        resetInterval();
+    } else {
+        changeImg(currImg - 1);
+        resetInterval();
+    }
+}
+
+function nextImage() {
+    if (currImg === imgs.length - 1) {
+        changeImg(0);
+        resetInterval();
+    } else {
+        changeImg(currImg + 1);
+        resetInterval();
+    }
+}
+
+var prevButton = document.getElementById("prev");
+prevButton.addEventListener("click", previousImage);
+
+var nextButton = document.getElementById("next");
+nextButton.addEventListener("click", nextImage);
